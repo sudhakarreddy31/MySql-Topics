21e4554cbf689e132f9efaeae20df072a783e1d0
sudhakarreddy <sudhakarreddymoola31@gmail.com>
2023-10-19 14:20:44 +0530

    Created New Database in Mysql

---
+++ DataBase.sql
@@ -0,0 +1,18 @@
+-- Create New Data Base In MySql 
+
+
+create database TestDB;          ---> TestDB Is Data Base Name 
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+     
+| TestDB             |
+| information_schema |
+| mysql              |
+| performance_schema |
+| sys                |
++--------------------+
+
+use TestDB;
+
